09dfc30f07cf5a9675489db72be3fd51e5a04fd0
dlamb22 <domlambwebdev@gmail.com>
2021-05-15 06:35:54 -0500

    pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,11 +6,12 @@
         @foreach ($posts as $post)
             <div class="card my-3">
                 <div class="card-body">
-                    <h3>{{ $post->title }}</h3>
+                    <h3><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h3>
                     <small>Created on {{ $post->created_at }}</small>
                 </div>
             </div>
         @endforeach
+        {{ $posts->links() }}
     @else
         <p>No posts found.</p>
     @endif
